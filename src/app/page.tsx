46debea58cf35819134c11a8cee92791b333e7f9
syaring <sarah.k.serim@gmail.com>
2024-03-14 21:24:38 +0900

    tmp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,17 @@ type TypeGrape = {
   status: typeof NONE | typeof DONE | typeof HALF;
 };
 
+// 대상 시트
+// https://docs.google.com/spreadsheets/d/1azYhHUyle5Y1hqGkR5xhHtt1nUm6PlYuyw4_2U7J7K4/edit#gid=0
+// 스프레드시트: 1azYhHUyle5Y1hqGkR5xhHtt1nUm6PlYuyw4_2U7J7K4
+// 시트: 0
+// 이름: [알파벳]1
+// 개인 수행 날짜: [알파벳]2 ~ 32
+// 총 합계: [알파벳]33
+
+// https://developers.google.com/sheets/api/guides/concepts?hl=ko
+// https://console.cloud.google.com/apis/api/sheets.googleapis.com/metrics?project=grape-job
+
 export default function Home() {
   const grapes: TypeGrape[] = new Array(31).fill(0).map((_, idx) => ({ id: idx, day: idx + 1, status: NONE }));
 
