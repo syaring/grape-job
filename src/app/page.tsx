3f17c115ea3a2b24eb3342d8e31919fc996b8736
syaring <sarah.k.serim@gmail.com>
2024-05-11 14:15:33 +0900

    SpeedInsights 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Grapes from "@/components/Grapes";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default async function Home() {
   return (
-    <div>
-      <div id="title">
-        🍇 포도알 5기 🍇
+      <div>
+        <div id="title">
+          🍇 포도알 5기 🍇
+        </div>
+        <Grapes />
+        <SpeedInsights />
       </div>
-      <Grapes />
-    </div>
   );
 }
