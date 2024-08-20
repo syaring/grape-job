a69912638a6a873e52e34f92269a805065191bf1
syaring <sarah.k.serim@gmail.com>
2024-08-20 11:00:40 +0900

    포도알 기수 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
       <div>
         <div id="title">
-          🍇 포도알 5기 🍇
+          🍇 포도알 6기 🍇
         </div>
         <Grapes />
         <SpeedInsights />
